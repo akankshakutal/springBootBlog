4c623dafa972c3dbe7c6c065ce27903482569719
Akanksha Kutal <akankshk@In-B3-Akankshk.local>
2019-06-18 19:08:03 +0530

    Create header Component with title and date

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,19 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = ({ siteTitle, date }) => (
+  <header className="header">
+    <div>
+      <h1>{siteTitle}</h1>
+      <div className="date">Updated at {date}</div>
     </div>
   </header>
 )
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
+  date: PropTypes.string,
 }
 
 Header.defaultProps = {
