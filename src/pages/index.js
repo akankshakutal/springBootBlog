2495504210e0e15263ed25ca7b4b8f49934b4c92
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-09-06 19:29:47 +0530

    Used static query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,32 @@
 import React from "react"
 import Layout from "../components/layout"
+import { StaticQuery, graphql } from "gatsby"
 
-const IndexPage = ({ data }) => <Layout data={data}></Layout>
-
-export const query = graphql`
-  query MyQuery {
-    allDataJson {
-      edges {
-        node {
-          title
-          date
-          content {
-            text
-            topic
-            subtopics {
-              text
-              topic
-              codeSnippet
+const IndexPage = () => (
+  <StaticQuery
+    query={graphql`
+      query MyQuery {
+        allDataJson {
+          edges {
+            node {
+              title
+              date
+              content {
+                text
+                topic
+                subtopics {
+                  text
+                  topic
+                  codeSnippet
+                }
+              }
             }
           }
         }
       }
-    }
-  }
-`
+    `}
+    render={data => <Layout data={data}></Layout>}
+  />
+)
 
 export default IndexPage
