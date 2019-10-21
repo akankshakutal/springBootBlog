ff70910ba68becf6fc05351b5f1fb52fbc73a71a
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-10-21 08:56:02 +0530

    Added notes for JVM memory management

--- src/components/content.js
+++ src/components/content.js
@@ -2,15 +2,23 @@ import PropTypes from "prop-types"
 import React from "react"
 import "./content.css"
 import SubTopic from "./subTopic"
+import Figure from "./figure"
 
 const Content = ({ content }) => {
-  let subtopics = content.subtopics.map(subtopic => {
-    return <SubTopic content={subtopic}></SubTopic>
-  })
+  let subtopics
+  const diagram = content.diagramPath ? (
+    <Figure figurePath={content.diagramPath}></Figure>
+  ) : null
+  if (content.subtopics) {
+    subtopics = content.subtopics.map(subtopic => {
+      return <SubTopic content={subtopic}></SubTopic>
+    })
+  }
   return (
     <div>
       <h2>{content.topic}</h2>
       <pre>{content.text}</pre>
+      <div>{diagram}</div>
       {subtopics}
     </div>
   )
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,26 +3,19 @@ import React from "react"
 import "./layout.css"
 import Header from "./header"
 import Content from "./content"
-import Figure from "./figure"
 
 const Layout = ({ data }) => {
   let date = data.allDataJson.edges[0].node.date
   let title = data.allDataJson.edges[0].node.title
   let content = data.allDataJson.edges[0].node.content
-  console.log(content[0].diagramPath)
+  let contentElement = content.map(data => {
+    return <Content content={data}></Content>
+  })
+
   return (
     <div>
       <Header siteTitle={title} date={date} />
-      <div className="main-container">
-        <Content content={content[0]}></Content>
-        <Figure figurePath={content[0].diagramPath}></Figure>
-        <Content content={content[1]}></Content>
-        <Content content={content[2]}></Content>
-        <Figure figurePath={content[2].diagramPath}></Figure>
-        <Content content={content[3]}></Content>
-        <Content content={content[4]}></Content>
-        <Content content={content[5]}></Content>
-      </div>
+      <div className="main-container">{contentElement}</div>
     </div>
   )
 }
